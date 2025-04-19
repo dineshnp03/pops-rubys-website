3c8e68bffcf475763b749837845a77d3c3c2875c
Sumit <red.fyrd@gmail.com>
2025-04-18 21:36:44 -0400

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-// Author name: Dinesh Narasimhalu Punniyamoorthy
+// Author: Dinesh Narasimhalu Punniyamoorthy
 import React from "react";
 import "../css/styles.css";
 import logo from "../images/logo.png";
@@ -6,15 +6,55 @@ import logo from "../images/logo.png";
 function Footer() {
   return (
     <footer className="footer">
-      <img
-        src={logo}
-        alt="Pops and Ruby's Logo"
-        width="40"
-        height="40"
-        className="mb-2"
-      />
-      <p>&copy; 2025 Pops and Ruby's. All rights reserved.</p>
-      <p>Follow us on social media @PopsAndRubys</p>
+      <div className="footer-container">
+        {/* Branding */}
+        <div className="footer-column">
+          <img src={logo} alt="Pops and Ruby's Logo" className="footer-logo" />
+          <p className="footer-title">Pops and Ruby's Tap & Grill</p>
+          <p>Cambridge, Ontario</p>
+        </div>
+
+        {/* Contact Info */}
+        <div className="footer-column">
+          <h4>Contact</h4>
+          <p>📍 200 Preston Pkwy, Cambridge, ON N3H 5N1</p>
+          <p>📞 (519) 653-4814</p>
+          <p>🕒 Daily: 11:00 AM – 11:00 PM</p>
+        </div>
+
+        {/* Social Media */}
+        <div className="footer-column">
+          <h4>Connect</h4>
+          <p>
+            <a
+              href="https://www.facebook.com/137675976246999/"
+              target="_blank"
+              rel="noopener noreferrer">
+              Facebook
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://www.instagram.com/explore/locations/297984953/pops-rubys-tap-grill/"
+              target="_blank"
+              rel="noopener noreferrer">
+              Instagram
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://www.skipthedishes.com/pops-rubys"
+              target="_blank"
+              rel="noopener noreferrer">
+              Order Online
+            </a>
+          </p>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        &copy; {new Date().getFullYear()} Pops and Ruby's. All rights reserved.
+      </div>
     </footer>
   );
 }
